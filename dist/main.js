fb7df074f530fd8a2319d5517850e7a8a48562f9
tzlilhaim <tzlilhaim480@gmail.com>
2020-08-26 17:58:02 +0300

    fixed bugs and added features

--- dist/main.js
+++ dist/main.js
@@ -1,42 +1,52 @@
 // BAD PRACTICE - not proper MVC. Should be separated to files.
 const render = function (todos) {
-
-    $("#todos").empty()
-
-    todos.forEach(todo => {
-        $("#todos").append(`
-        <div data-id=${todo._id} class="todo ${todo.complete ? 'complete' : ''}">
-            <i class="fas fa-check-circle"></i>
-            <span class=text>todo.text</span>
-            <span class="delete"><i class="fas fa-trash"></i></span>
-        </div>
-        `)
-    })
+  $("#todos").empty()
+    const source = $("#todo-template").html()
+    const template = Handlebars.compile(source)
+    const newHTML = template({todos})
+    $("#todos").append(newHTML)
 }
 
 const add = function () {
-    $.post('/todo', { text: $("#todo-input").val() }, function (todos) {
-        render(todos)
-        $("#todo-input").val("")
+  if ($("#todo-input").val()) {
+    $.post("/todo", { text: $("#todo-input").val() }, function (todosArr) {
+      render(todosArr.todos)
+      $("#todo-input").val("")
     })
+  } else {
+    alert("Must insert input!!!")
+  }
 }
 
 $("#todos").on("click", ".fa-check-circle", function () {
-    const id = $(this).closest(".todo").data().id
-    $.ajax({
-        method: "PUT",
-        url: "/todo/" + id,
-        success: todos => render(todos)
-    })
+  const id = $(this).closest(".todo").data().id
+  $.ajax({
+    method: "PUT",
+    url: "/todo/" + id + "/toggleComplete",
+    success: (todosArr) => {
+      render(todosArr.todos)
+    },
+  })
 })
 
 $("#todos").on("click", ".fa-trash", function () {
-    const id = $(this).closest(".todo").data().id
-    $.ajax({
-        method: "DELETE",
-        url: "/todo/" + id,
-        success: todos => render(todos)
-    })
+  const id = $(this).closest(".todo").data().id
+  $.ajax({
+    method: "DELETE",
+    url: "/todo/" + id,
+    success: (todosArr) => render(todosArr.todos),
+  })
 })
 
-$.get('/todos', todos => render(todos))
+$("#todos").on("click", ".text", function () {
+    const id = $(this).closest(".todo").data().id
+  $.ajax({
+    method: "PUT",
+    url: "/todo/" + id + "/upgradePriority",
+    success: (todosArr) => render(todosArr.todos),
+  })
+})
+
+$.get("/todos", (todosArr) => {
+  render(todosArr.todos)
+})
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,33 +1,68 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 
-const todos = []
-let id = 1
+class ToDosArr {
+  constructor() {
+    this.todos = []
+    this.idCount = 0
+  }
+  addToDo(todo) {
+    this.todos.push(todo)
+    this.idCount++
+  }
+  removeToDo(todo) {
+    const todoIndex = this.todos.indexOf(todo)
+    this.todos.splice(todoIndex, 1)
+  }
+}
 
-router.get('/todos', function (req, res) {
-    res.send(todos)
+class ToDo {
+  constructor(id, text) {
+    this.id = id
+    this.text = text
+    this.complete = false
+    this.priority = 1
+  }
+  toggleComplete() {
+    this.complete = !this.complete
+  }
+  upgradePriority(){
+    if(this.priority === 3){
+        this.priority = 1
+    }else{
+        this.priority++
+    }
+  }
+}
+
+const todosArr = new ToDosArr()
+
+router.get("/todos", function (req, res) {
+  res.send(todosArr)
 })
 
-router.post('/todo', function (req, res) {
-    const text = req.body.text
-    const newTodo = { id: id++, text: text, complete: false }
-
-    todos.push(newTodo)
-    res.send(todos)
+router.post("/todo", function (req, res) {
+  const text = req.body.text
+  todosArr.addToDo(new ToDo(todosArr.idCount, text))
+  res.send(todosArr)
 })
 
-router.put('/todo/:todoID', function (req, res) {
+router.put("/todo/:todoID/toggleComplete", function (req, res) {
+  const todoID = req.params.todoID
+  todosArr.todos.find((t) => t.id == todoID).toggleComplete()
+  res.send(todosArr)
+})
+
+router.put("/todo/:todoID/upgradePriority", function (req, res) {
     const todoID = req.params.todoID
+    todosArr.todos.find((t) => t.id == todoID).upgradePriority()
+    res.send(todosArr)
+  })
 
-    todos.find(t => t.id == todoID).completed = true
-    res.send(todos)
-})
-
-router.delete('/todo/:todoID', function (req, res) {
-    const todoID = req.params.todoID
-    todos.splice(todoID, 1)
-    console.log('hello')
-    res.send(todos)
+router.delete("/todo/:todoID", function (req, res) {
+  const todoID = req.params.todoID
+  todosArr.removeToDo(todosArr.todos.find((t) => t.id == todoID))
+  res.send(todosArr)
 })
 
 module.exports = router
